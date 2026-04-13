fbd89627053b164ef52d0a053f61b300fe95ee77
deton24 <34845212+deton24@users.noreply.github.com>
2026-04-13 17:27:14 +0200

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,202 @@
+# coding: utf-8
+__author__ = 'Roman Solovyev (ZFTurbo): https://github.com/ZFTurbo/'
+
+import time
+import librosa
+import sys
+import os
+import glob
+import torch
+import soundfile as sf
+import numpy as np
+from tqdm.auto import tqdm
+import torch.nn as nn
+
+# Using the embedded version of Python can also correctly import the utils module.
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
+
+from utils.audio_utils import normalize_audio, denormalize_audio, draw_spectrogram
+from utils.settings import get_model_from_config, parse_args_inference
+from utils.model_utils import demix
+from utils.model_utils import prefer_target_instrument, apply_tta, load_start_checkpoint
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def run_folder(model, args, config, device, ckpt_name, verbose: bool = False):
+    """
+    Process a folder of audio files for source separation.
+
+    Parameters:
+    ----------
+    model : torch.nn.Module
+        Pre-trained model for source separation.
+    args : Namespace
+        Arguments containing input folder, output folder, and processing options.
+    config : Dict
+        Configuration object with audio and inference settings.
+    device : torch.device
+        Device for model inference (CPU or CUDA).
+    verbose : bool, optional
+        If True, prints detailed information during processing. Default is False.
+    """
+
+    model.eval()
+
+    mixture_paths = sorted(glob.glob(os.path.join(args.input_folder, '*.*')))
+    sample_rate = getattr(config.audio, 'sample_rate', 44100)
+
+    print(f"Total files found: {len(mixture_paths)}. Using sample rate: {sample_rate}")
+
+    instruments = prefer_target_instrument(config)[:]
+    os.makedirs(args.store_dir, exist_ok=True)
+
+    if not verbose:
+        mixture_paths = tqdm(mixture_paths, desc="Total progress")
+
+    if args.disable_detailed_pbar:
+        detailed_pbar = False
+    else:
+        detailed_pbar = True
+
+    for path in mixture_paths:
+        start_time = time.time()
+        print(f"Processing track: {path}")
+        try:
+            mix, sr = librosa.load(path, sr=sample_rate, mono=('model' in config and 'stereo' in config.model and not config.model['stereo']))
+        except Exception as e:
+            print(f'Cannot read track: {format(path)}')
+            print(f'Error message: {str(e)}')
+            continue
+
+        # If mono audio we must adjust it depending on model
+        if len(mix.shape) == 1:
+            mix = np.expand_dims(mix, axis=0)
+            if 'num_channels' in config.audio:
+                if config.audio['num_channels'] == 2:
+                    print(f'Convert mono track to stereo...')
+                    mix = np.concatenate([mix, mix], axis=0)
+
+        mix_orig = mix.copy()
+        if 'normalize' in config.inference:
+            if config.inference['normalize'] is True:
+                mix, norm_params = normalize_audio(mix)
+
+        instruments = prefer_target_instrument(config)[:]
+        
+        waveforms_orig = demix(config, model, mix, device, model_type=args.model_type, pbar=detailed_pbar)
+
+        if args.use_tta:
+            waveforms_orig = apply_tta(config, model, mix, waveforms_orig, device, args.model_type)
+
+        if args.demud_phaseremix_inst:
+            print(f"Demudding track (phase remix - instrumental): {path}")
+            instr = 'vocals' if 'vocals' in instruments else instruments[0]
+            instruments.append('instrumental_phaseremix')
+            if 'instrumental' not in instruments and 'Instrumental' not in instruments:
+                mix_modified = mix_orig - 2*waveforms_orig[instr]
+                mix_modified_ = mix_modified.copy()
+
+                waveforms_modified = demix(config, model, mix_modified, device, model_type=args.model_type, pbar=detailed_pbar)
+                if args.use_tta:
+                    waveforms_modified = apply_tta(config, model, mix_modified, waveforms_modified, device, args.model_type)
+
+                waveforms_orig['instrumental_phaseremix'] = mix_orig + waveforms_modified[instr]
+            else:
+                mix_modified = 2*waveforms_orig[instr] - mix_orig
+                mix_modified_ = mix_modified.copy()
+
+                waveforms_modified = demix(config, model, mix_modified, device, model_type=args.model_type, pbar=detailed_pbar)
+                if args.use_tta:
+                    waveforms_modified = apply_tta(config, model, mix_modified, waveforms_orig, device, args.model_type)
+
+                waveforms_orig['instrumental_phaseremix'] = mix_orig + mix_modified_ - waveforms_modified[instr]
+        
+        if args.extract_instrumental:
+            instr = 'vocals' if 'vocals' in instruments else instruments[0]
+            if 'instrumental' not in instruments and 'Instrumental' not in instruments:
+                instruments.append('instrumental')
+                waveforms_orig['instrumental'] = mix_orig - waveforms_orig[instr]
+            else:
+                instruments.append('other')
+                waveforms_orig['other'] = mix_orig - waveforms_orig[instr]
+
+        for instr in instruments:
+            estimates = waveforms_orig[instr]
+            if 'normalize' in config.inference:
+                if config.inference['normalize'] is True:
+                    estimates = denormalize_audio(estimates, norm_params)
+
+            file_name, _ = os.path.splitext(os.path.basename(path))
+            if args.use_prefix:
+                file_name = f"\ufa6c{file_name}"
+            if args.flac_file:
+                output_file = os.path.join(args.store_dir, f"{file_name}_{ckpt_name}{instr}.flac")
+                subtype = 'PCM_16' if args.pcm_type == 'PCM_16' else 'PCM_24'
+                sf.write(output_file, estimates.T, sr, subtype=subtype)
+            else:
+                output_file = os.path.join(args.store_dir, f"{file_name}_{ckpt_name}{instr}.wav")
+                sf.write(output_file, estimates.T, sr, subtype='FLOAT')
+
+            if args.draw_spectro > 0:
+                output_img_path = os.path.join(args.store_dir, f"{file_name}_{ckpt_name}{instr}.jpg")
+                draw_spectrogram(estimates.T, sr, args.draw_spectro, output_img_path)
+
+        print("Done processing: {:.2f} sec".format(time.time() - start_time))
+        time.sleep(1)
+
+
+def proc_folder(dict_args):
+    args = parse_args_inference(dict_args)
+    device = "cpu"
+    if args.force_cpu:
+        device = "cpu"
+    elif torch.cuda.is_available():
+        print('CUDA is available, use --force_cpu to disable it.')
+        device = f'cuda:{args.device_ids[0]}' if isinstance(args.device_ids, list) else f'cuda:{args.device_ids}'
+    elif torch.backends.mps.is_available():
+        device = "mps"
+
+    print("Using device: ", device)
+
+    model_load_start_time = time.time()
+    torch.backends.cudnn.benchmark = True
+
+    model, config = get_model_from_config(args.model_type, args.config_path)
+
+    if args.start_check_point != '':
+        if args.num_overlap>0:
+            config.inference.num_overlap = args.num_overlap
+        if args.chunk_size>0:
+            config.audio.chunk_size = args.chunk_size
+        checkpoint = torch.load(args.start_check_point, weights_only=False, map_location='cpu')
+        load_start_checkpoint(args, model, checkpoint, type_='inference')
+
+    print("Instruments: {}".format(config.training.instruments))
+
+    # in case multiple CUDA GPUs are used and --device_ids arg is passed
+    if isinstance(args.device_ids, list) and len(args.device_ids) > 1 and not args.force_cpu:
+        model = nn.DataParallel(model, device_ids=args.device_ids)
+
+    model = model.to(device)
+
+    print("Model load time: {:.2f} sec".format(time.time() - model_load_start_time))
+
+    ckpt_name = ''
+    if args.use_modelname:
+        ckpt_name, _ = os.path.splitext(os.path.basename(args.start_check_point))
+        ckpt_name += '_'
+    if args.use_modelconf:
+        if 'num_overlap' in config.inference.keys():
+            ckpt_name += f"o{config.inference.num_overlap:02}_"
+        if 'chunk_size' in config.audio.keys():
+            ckpt_name += f"c{config.audio.chunk_size//10000}w_"
+    time.sleep(0.8)
+    run_folder(model, args, config, device, ckpt_name, verbose=True)
+
+
+if __name__ == "__main__":
+    proc_folder(None)
